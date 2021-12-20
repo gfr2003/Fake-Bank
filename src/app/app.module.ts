d912f6fe537db7628a2d1bbcdfb02c355a835cb4
Gustavo Felix <gu2003fr@outlook.com>
2021-12-20 14:52:44 -0300

    feat - new format money and date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NewTransferComponent } from './new-transfer/new-transfer.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { IConfig, NgxMaskModule } from 'ngx-mask';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 
+registerLocaleData(localePt, 'pt');
 @NgModule({
-  declarations: [AppComponent, NewTransferComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [],
+  declarations: [AppComponent, NewTransferComponent, ExtratoComponent],
+  imports: [BrowserModule, FormsModule, NgxMaskModule.forRoot()],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
